ee5385c499c86802488cc305122ded11948a8727
sametcn99 <sametcn99@gmail.com>
2023-11-06 21:39:57 +0300

    supabase integration

---
+++ src/app/auth/sign-up/route.ts
@@ -0,0 +1,40 @@
+import { createClient } from "../../../../utils/supabase/server"
+import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
+
+export async function POST(request: Request) {
+  const requestUrl = new URL(request.url);
+  const formData = await request.formData();
+  const email = String(formData.get("email"));
+  const password = String(formData.get("password"));
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+
+  // Create a new user with their email and password
+  const { error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      // This is where the user will be redirected after signing up
+      emailRedirectTo: `${requestUrl.origin}/auth/callback`,
+    },
+  });
+
+  if (error) {
+    return NextResponse.redirect(
+      `${requestUrl.origin}/login?error=Could not authenticate user`,
+      {
+        // a 301 status is required to redirect from a POST to a GET route
+        status: 301,
+      },
+    );
+  }
+
+  return NextResponse.redirect(
+    `${requestUrl.origin}/login?message=Check email to continue sign in process`,
+    {
+      // a 301 status is required to redirect from a POST to a GET route
+      status: 301,
+    },
+  );
+}
---
+++ utils/supabase/server.ts
@@ -0,0 +1,36 @@
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { cookies } from "next/headers";
+
+// 1. Create the Supabase client with the URL and key from the .env file
+export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      // 2. Tell Supabase how to read and write cookies
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value;
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          try {
+            cookieStore.set({ name, value, ...options });
+          } catch (error) {
+            // The `set` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+        remove(name: string, options: CookieOptions) {
+          try {
+            cookieStore.delete({ name, ...options });
+          } catch (error) {
+            // The `delete` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+      },
+    },
+  );
+};
