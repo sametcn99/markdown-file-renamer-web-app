d3600dd707ce6cfbe6dbe752189ddafdabf88216
samet <sametcn99@gmail.com>
2024-02-29 23:57:18 +0300

    seo

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <p className="bottom-4 text-center">
       Check out the source code on {/* GitHub link with styling */}
       <a
-        href="https://github.com/sametcn99/markdown-file-renamer-web-app"
+        href="https://sametcc.me/markdown-file-renamer-web-app"
         className="text-blue-600"
         target="_blank"
         rel="noreferrer"
